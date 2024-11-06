b76495ea126197dc4cdf1fd7700c802c397becaf
Gia-gia9 <giadagelashvili@gmail.com>
2024-11-06 21:48:22 +0400

    sum

---
+++ script.js
@@ -0,0 +1,28 @@
+let number = 11
+
+if (number**2 > 100) {
+    console.log("metia asze");
+} else {
+    console.log("naklebia asze");
+}
+
+
+
+let number2 = 75;
+
+if (number2 > 50 && number2 < 100) {
+    console.log("metia 50ze da naklebia 100ze");
+} else {
+    console.log("naklebia 50ze");
+}
+
+
+
+let number3 = -2;
+
+if (number3 <= 0) {
+    console.log("naklebia an tolia 0ze");
+} else {
+    console.log("metia 0ze");
+}
+
